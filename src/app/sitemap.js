5150a53e1b0c35a8606d0d26bd0e0a3c7fb9ba33
Ankit Tiwari <ankittiwari2282000@gmail.com>
2026-02-07 22:52:45 +0530

    SEO: add sitemap and robots for Next.js

---
+++ src/app/sitemap.js
@@ -0,0 +1,34 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://www.manozensolution.com',
+      lastModified: new Date(),
+      priority: 1.0,
+    },
+    {
+      url: 'https://www.manozensolution.com/services',
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.manozensolution.com/otherservice',
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.manozensolution.com/our-process',
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.manozensolution.com/about',
+      lastModified: new Date(),
+      priority: 0.7,
+    },
+    {
+      url: 'https://www.manozensolution.com/contact',
+      lastModified: new Date(),
+      priority: 0.7,
+    },   
+  ];
+}
