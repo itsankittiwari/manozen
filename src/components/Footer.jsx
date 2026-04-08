a57aca4c564992905ddbe463543a933258f2d66f
Ankit Tiwari <ankittiwari2282000@gmail.com>
2026-04-08 20:58:43 +0530

    [IMP] Add the office address in website

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -53,10 +53,10 @@ export default function Footer() {
                 <Phone size={16} />
                 <span>+91 8178324532</span>
               </li>
-              {/* <li className="flex items-start space-x-2">
+              <li className="flex items-start space-x-2">
                 <MapPin size={16} className="mt-1" />
-                <span>N-103 Sector 25, Noida</span>
-              </li> */}
+                <span>N-103 Sector 25, Noida, Gautam Buddh Nagar, UP - 201301</span>
+              </li>
             </ul>
           </div>
         </div>
